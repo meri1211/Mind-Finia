b5f5002fbca2b71ee47e81283d5c1a1c0b321c61
meri <mariana_asavoaiei@outlook.com>
2023-10-11 23:46:10 +0100

    created a more complex folder structure and the 3 pages we are using

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 
-
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
-  return (
+  return ( <>
     <div className="App">
-      
+      <Header>
+        </Navbar>
+      </Header>
+      </AnimatedDiv>
+      </PictureDiv>
+      </AnimatedLogosContainer>
+      </ServicesDiv>
+      </PictureDiv>
+      </GravityInfoSolutions>
+      </ValuedCustomer>
+      </Reviews>
+      </Workflow>
+      </Footer>
     </div>
+  </>
   );
 }
 
