e4971fd3ee65e20cd20569bcafbce172e8fe6e4f
meri <mariana_asavoaiei@outlook.com>
2023-10-12 03:02:57 +0100

    created the Header component and the Navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
+/*import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Components/Navbar";*/
 
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header>
+          <Navbar />
+        </Header>
+        <Switch>
+          <Route exact path="/" component={Homepage}>
+            <AnimatedDiv />
+            <AnimatedLogosContainer />
+            <ServicesDiv />
+            <GravityInfoSolutions />
+            <ValuedCustomer />
+            <Reviews />
+            <Workflow />
+          </Route>
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/info-table" component={InfoTable} />
+        </Switch>
+        <PictureDiv />
+        <PictureDiv />
+        <Footer />
+      </div>
+    </Router>
+  );
+}*/
 
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Homepage from "./Homepage";
+import AboutUs from "./AboutUs";
+import InfoTable from "./InfoTable";
 
 function App() {
-  return ( <>
-    <div className="App">
-      <Header>
-        </Navbar>
-      </Header>
-      </AnimatedDiv>
-      </PictureDiv>
-      </AnimatedLogosContainer>
-      </ServicesDiv>
-      </PictureDiv>
-      </GravityInfoSolutions>
-      </ValuedCustomer>
-      </Reviews>
-      </Workflow>
-      </Footer>
-    </div>
-  </>
+  return (
+    <Router>
+      <div className="App">
+        <Header>
+          <Navbar />
+        </Header>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/info-table" component={InfoTable} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
